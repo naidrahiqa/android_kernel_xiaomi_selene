74c51a2d5027299a6ba3d2f7babcd5860764e84a
naidrahiqa <akufaqih1@gmail.com>
2026-07-24 05:36:06 +0700

    fix: add xt_connmark_tginfo2 and D_SHIFT_RIGHT to xt_connmark.h

--- include/uapi/linux/netfilter/xt_connmark.h
+++ include/uapi/linux/netfilter/xt_connmark.h
@@ -4,15 +4,35 @@
 
 #include <linux/types.h>
 
+/* Copyright (C) 2002,2004 MARA Systems AB <http://www.marasystems.com>
+ * by Henrik Nordstrom <hno@marasystems.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
 enum {
 	XT_CONNMARK_SET = 0,
 	XT_CONNMARK_SAVE,
 	XT_CONNMARK_RESTORE
 };
 
+enum {
+	D_SHIFT_LEFT = 0,
+	D_SHIFT_RIGHT,
+};
+
 struct xt_connmark_tginfo1 {
 	__u32 ctmark, ctmask, nfmask;
 	__u8 mode;
+};
+
+struct xt_connmark_tginfo2 {
+	__u32 ctmark, ctmask, nfmask;
+	__u8 mode;
+	__u8 shift_dir, shift_bits, __pad0;
 };
 
 struct xt_connmark_mtinfo1 {
